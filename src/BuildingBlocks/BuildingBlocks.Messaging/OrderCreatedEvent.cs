adcb0feb395ce5ae97bbd65c59db039ba3300f0a
Kaixuan Khoo <kkh@redwoodtech.com>
2026-03-25 16:52:54 +0000

    Init project, services, and docker-compose

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/OrderCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Messaging;
+
+public record OrderCreatedEvent
+{
+    public Guid OrderId { get; init; }
+    public decimal Amount { get; init; }
+    public string CustomerEmail { get; init; } = string.Empty;
+    public DateTime CreatedAtUtc { get; init; }
+    public Guid CorrelationId { get; init; }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/PaymentSucceededEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingBlocks.Messaging;
+
+public record PaymentSucceededEvent
+{
+    public Guid OrderId { get; init; }
+    public Guid PaymentId { get; init; }
+    public decimal Amount { get; init; }
+    public string CustomerEmail { get; init; } = string.Empty;
+    public DateTime TimeStamp { get; init; }
+    public Guid CorrelationId { get; init; }
+}
