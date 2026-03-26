6ca7d8fc95c8620e380fc501e013308c4f641eee
Kaixuan Khoo <kkh@redwoodtech.com>
2026-03-26 21:58:32 +0000

    Api Gateway

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,6 +1,24 @@
+using BuildingBlocks.Messaging.Extensions;
+using BuildingBlocks.Messaging.Middleware;
+using Serilog;
+using Yarp.ReverseProxy;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddSerilogLogging("ApiGateway");
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+app.UseMiddleware<CorrelationIdMiddleware>();
+app.UseSerilogRequestLogging();
+
+app.MapReverseProxy();
+app.MapHealthChecks("/health");
 
 app.Run();
